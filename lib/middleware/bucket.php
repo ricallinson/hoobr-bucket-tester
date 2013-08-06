1bd84bd294486c7e5cd6175e5b5188c3b96c59eb
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-05 17:20:42 -0700

    Added dirty hashing algo

--- lib/middleware/bucket.php
+++ lib/middleware/bucket.php
@@ -1,3 +1,21 @@
 <?php
 namespace php_require\hoobr_bucket_tester;
 
+$req = $require("php-http/request");
+$res = $require("php-http/response");
+$uuid = $require("php-uuid");
+
+$tracker = $req->cookie("hoobrt");
+
+if (!$tracker) {
+    $tracker = $uuid->generate(4, 101);
+    $res->cookie("hoobrt", $tracker);
+}
+
+/*
+    Dirty bucket algo.
+*/
+
+$bucketId = round(crc32($tracker) / 100000000);
+
+// var_dump($bucketId);
